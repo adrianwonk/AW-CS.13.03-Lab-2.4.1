a2c2bcab8c0703a8c75dc279511cc199587da8dd
adrianwonk <adrianwong135@gmail.com>
2023-11-21 14:59:03 +0800

    finished

--- src/CISStack.java
+++ src/CISStack.java
@@ -2,19 +2,46 @@ import java.util.LinkedList;
 
 public class CISStack {
 
-    // Linked list property.
+    LinkedList<Integer> list;
+    int size;
+    public CISStack(){
+        list = new LinkedList<Integer>();
+        size = 0;
+    }
 
-    // Size property.
+    public void push(int value){
+        list.addLast(value);
+        size++;
+    }
+    public Integer pop(){
+        Integer result = list.getLast();
+        list.removeLast();
+        size--;
+        return result;
+    }
 
-    // Constructor.
+    public boolean isEmpty(){
+        return (size == 0);
+    }
 
-    // Push. This method pushes a value onto the top of the stack.
+    public int size (){
+        return size;
+    }
 
-    // Pop. This method pops a value from the top of the stack.
-
-    // isEmpty. Returns a boolean indicating whether the linked list is empty.
-
-    // size. Returns the size of the queue.
+    public String toString(){
+        if (isEmpty()){
+            return "CISStack{stack=[], size=0}";
+        }
+        else {
+            String result = "CISStack{stack=[";
+            for (var value : list){
+                result += "" + value + ", ";
+            }
+            result = result.substring(0,result.length()-2);
+            result += "], size=" + size + "}";
+            return result;
+        }
+    }
 
     // toString. Returns a description of the queue in, for example, the following format:
     // CISStack{stack=[7, 11], size=2}
